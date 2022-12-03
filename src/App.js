cfe838470b3ac3d5fe12df7ccbf11449e3af2ad0
Mohanad <mohannad@birdie.ps>
2022-12-03 14:17:29 +0200

    Changed name

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app">
+
+      <header className="app__header">
+        Mohanad Atrash
+      </header>
+
+      <main className="app__main">
+      </main>
+
+    </div>
+  );
+}
+
+
+export default App;
